d126d6c3a932a8429bd586b4cde3970df030505b
kait-pl-testing <kaitcart@Kaitlins-MBP.corp.puppetlabs.net>
2016-02-08 13:43:43 -0800

    added package resource

--- puppet.pp
+++ puppet.pp
@@ -1,5 +1,9 @@
 file {'foo':
-ensure => present,
-mode => 0644,
-content => "testing git",
+  ensure => present,
+  mode => 0644,
+  content => "testing git",
+}
+
+package { 'bar':
+  ensure => present,
 }
