381a7ac3d2ed3da0a395f9a3fecdfc04f0495149
kaitlincarter <kaitlin@puppetlabs.com>
2016-02-10 15:30:00 -0800

    Update test.pp

--- test.pp
+++ test.pp
@@ -1,10 +1,8 @@
 class ntp::test{
-
-
     file { 'test':
       ensure => present,
       path => "/etc/puppetlabs/code/environments/production/test.pp",
-      content => "hello world! facter $::up_time and  $::globvar and $::cat",
+      content => "hello world! $::globvar and $::cat",
     }
 }
 
